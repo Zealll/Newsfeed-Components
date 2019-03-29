af03bd9e335890394fcb58114ca5eea670ac8eeb
Zealll <eriznis@gmail.com>
2019-03-29 02:55:55 -0400

    expand button changes to close button once clicked

--- Article/Article.js
+++ Article/Article.js
@@ -1,23 +1,51 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
+// class Article {
+//   constructor(domElement) {
+//     // assign this.domElement to the passed in domElement
+//     this.domElement = domElement;
+//     // create a reference to the ".expandButton" class. 
+//     this.expandButton = domElement.querySelector('.expandButton')
+//     // Using your expandButton reference, update the text on your expandButton to say "expand"
+//     this.expandButton.textContent = 'expand'
+//     // Set a click handler on the expandButton reference, calling the expandArticle method.
+//     this.expandButton.addEventListener('click', () => this.expandArticle())
+
+//     this.closeButton = domElement.querySelector('.closeButtonClose')
+//     this.closeButton.addEventListener('click', () => this.expandArticle())
+//   }
+
+//   expandArticle() {
+//     // Using our reference to the domElement, toggle a class to expand or hide the article.
+//     this.domElement.classList.toggle('article-open')
+//     this.domElement.classList.toggle('closeButtonClose')
+//   }
+// }
+
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = domElement.querySelector('.expandButton')
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand'
+    this.expandButton.innerHTML = "Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle())
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open')
+    this.domElement.classList.toggle('article-open');
+    const close = this.expandButton
+    console.log(close)
+    if(close.innerHTML === "Expand") {
+      close.innerHTML = "Close"
+    } else {
+      close.innerHTML = "Expand"
+    }
   }
 }
-
 
 
 /* START HERE: 
